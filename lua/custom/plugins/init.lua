c36a9034de0750d2916eb6088e0f3bd47883e31c
ToastedToast <hey@toasted.dev>
2024-05-20 16:51:01 +0800

    feat(plugins): add oil.nvim plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,29 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'stevearc/oil.nvim',
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        view_options = {
+          show_hidden = true,
+        },
+      }
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'oil',
+        callback = function()
+          vim.opt_local.colorcolumn = ''
+          vim.opt_local.number = false
+        end,
+      })
+
+      vim.keymap.set('n', '<C-e>', function()
+        require('oil').toggle_float()
+      end)
+    end,
+  },
+}
