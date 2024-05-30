d161063faed5fdf3aa6118c3dc1e9ee6bd58a08a
ToastedToast <hey@toasted.dev>
2024-05-31 07:51:03 +0800

    some more things

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -1,7 +1,7 @@
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('toasted-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
