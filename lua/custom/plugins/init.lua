d4946e98a835287a440af6d32f616683ac3886b0
ToastedToast <hey@toasted.dev>
2024-05-31 16:26:02 +0800

    chore(plugins): fmt

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,6 +1,6 @@
 return {
   'tpope/vim-sleuth',
   { 'supermaven-inc/supermaven-nvim', opts = {} },
-  { 'andweeb/presence.nvim',          lazy = false },
-  { 'wakatime/vim-wakatime',          lazy = false },
+  { 'andweeb/presence.nvim', lazy = false },
+  { 'wakatime/vim-wakatime', lazy = false },
 }
