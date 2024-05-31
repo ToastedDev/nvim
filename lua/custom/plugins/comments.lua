9363ba096b87588ec5560f8384d6a9e09b010126
ToastedToast <hey@toasted.dev>
2024-05-31 08:34:40 +0800

    fix(comments): <C-/> keybind not working

--- lua/custom/plugins/comments.lua
+++ lua/custom/plugins/comments.lua
@@ -1,11 +1,20 @@
 return {
   {
     'numToStr/Comment.nvim',
+    keys = { '<C-_>', "<C-/>", 'gc', 'gcc' },
     config = function()
       require('Comment').setup()
+
+      vim.keymap.set('n', '<C-/>', function()
+        require('Comment.api').toggle.linewise.current()
+      end, { desc = 'Toggle Comment' })
+
       vim.keymap.set('n', '<C-_>', function()
         require('Comment.api').toggle.linewise.current()
       end, { desc = 'Toggle Comment' })
+
+      vim.keymap.set('v', '<C-/>', "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { desc = 'Toggle Comment' })
+
       vim.keymap.set('v', '<C-_>', "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { desc = 'Toggle Comment' })
     end,
   },
