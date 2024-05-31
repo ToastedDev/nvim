556a5d739e9bc865711dc0e49bf87750bd4228b4
ToastedToast <hey@toasted.dev>
2024-05-31 19:24:00 +0800

    feat(plugins): replace mini.statusline with lualine

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -8,18 +8,6 @@ return {
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       require('mini.surround').setup()
 
-      -- Simple and easy statusline.
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-
       -- Moving with Alt+J and Alt+K
       require('mini.move').setup()
 
