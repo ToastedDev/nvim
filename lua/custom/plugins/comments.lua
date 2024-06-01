574313e65ba69ae39d7b0614767c22c4478a06cd
ToastedToast <hey@toasted.dev>
2024-06-01 21:18:16 +0800

    fix(comment.nvim): use default keymaps

--- lua/custom/plugins/comments.lua
+++ lua/custom/plugins/comments.lua
@@ -1,34 +1,20 @@
 return {
   {
     'numToStr/Comment.nvim',
-    keys = { '<C-_>', '<C-/>', 'gc', 'gcc' },
-    config = function()
-      require('Comment').setup()
-
-      vim.keymap.set('n', '<C-/>', function()
-        require('Comment.api').toggle.linewise.current()
-      end, { desc = 'Toggle Comment' })
-
-      vim.keymap.set('n', '<C-_>', function()
-        require('Comment.api').toggle.linewise.current()
-      end, { desc = 'Toggle Comment' })
-
-      vim.keymap.set('v', '<C-/>', "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { desc = 'Toggle Comment' })
-
-      vim.keymap.set('v', '<C-_>', "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { desc = 'Toggle Comment' })
-    end,
+    keys = { 'gc', 'gcc', 'gb', 'gbc' },
+    opts = {},
   },
   {
     'folke/todo-comments.nvim',
     event = 'VimEnter',
-    keys = { "<leader>sc" },
+    keys = { '<leader>sc' },
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       require('todo-comments').setup {
         signs = false,
       }
 
-      vim.keymap.set('n', '<leader>sc', "<cmd>TodoTelescope<CR>", { desc = '[S]earch Todo [C]omments' })
+      vim.keymap.set('n', '<leader>sc', '<cmd>TodoTelescope<CR>', { desc = '[S]earch Todo [C]omments' })
     end,
   },
 }
