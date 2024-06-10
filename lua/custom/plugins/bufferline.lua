cdf96c12e25d8c8638551cb9e1b195cb6d0bfedd
ToastedToast <hey@toasted.dev>
2024-06-10 13:01:09 +0800

    feat(plugins): add bufferline plugin

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        mode = 'tabs',
+        always_show_bufferline = false,
+      },
+    },
+  },
+}
