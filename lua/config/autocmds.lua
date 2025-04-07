a1e6215dc430b8e937f7f2206d400f60338a4bb5
ToastedToast <hey@toasted.dev>
2025-04-07 09:39:58 +0800

    feat: some extra stuff

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,7 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.cmd([[
+  autocmd VimLeave * set guicursor= | call chansend(v:stderr, "\x1b[ q")
+]])
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,6 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    { "<leader>sf", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+  },
+}
