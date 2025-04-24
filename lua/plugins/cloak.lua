7f61d647af03c2de87f173060fdda844fa806e25
ToastedToast <hey@toasted.dev>
2025-04-24 09:40:01 +0800

    feat(plugins): add cloak

---
+++ lua/plugins/cloak.lua
@@ -0,0 +1,20 @@
+return {
+  'laytan/cloak.nvim',
+  event = 'VeryLazy',
+  config = function()
+    require('cloak').setup {
+      enabled = true,
+      cloak_character = '*',
+      highlight_group = 'Comment',
+      cloak_length = nil,
+      try_all_patterns = true,
+      patterns = {
+        {
+          file_pattern = '.env*',
+          cloak_pattern = '=".+"',
+          replace = nil,
+        },
+      },
+    }
+  end,
+}
